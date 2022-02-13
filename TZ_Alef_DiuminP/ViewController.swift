0aac83bc88a2d6b42550ad6f412508f6c3982ea6
DiuminPM <75372207+DiuminPM@users.noreply.github.com>
2022-02-13 21:48:52 +0300

    Initial Commit

---
+++ TZ_Alef_DiuminP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TZ_Alef_DiuminP
+//
+//  Created by DiuminPM on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
